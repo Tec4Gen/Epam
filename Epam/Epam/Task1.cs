38de8dd1cec4c6f374a0e68da896dcc4460b2367
Tec4Gen <steam.com77@yandex.ru>
2020-02-10 22:00:40 +0400

    Test Task

---
+++ Epam/Epam/Task1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Program
+{
+    //юзаем сортировку вставкой
+    static public void SortInsertion(int[] MyArray)
+    {
+        //Array.Sort(MyArray);//Так проще :3 
+
+        int Current;
+        int j;
+        for (int i = 1; i < MyArray.Length; i++)
+        {
+            Current = MyArray[i];
+            j = i;
+
+            while (j > 0 && Current < MyArray[j - 1])
+            {
+                MyArray[j] = MyArray[j - 1];
+                j--;
+            }
+            MyArray[j] = Current;
+        }
+    }
+    static public void ShowArray(int[] MyArray)
+    {
+        foreach (int item in MyArray)
+        {
+            Console.Write($"{item} ");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        //Random rdn = new Random();
+
+        Console.Write("Length: ");
+        uint.TryParse(Console.ReadLine(), out uint n);
+
+        int[] MyArray = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            //MyArray[i] = rdn.Next(-99999, 99999);//Так проще
+            int.TryParse(Console.ReadLine(), out MyArray[i]);
+        }
+
+        if (MyArray.Length > 1)//возможно лишние, но за то норм 
+        {
+            Console.Write("Не сортированный массив" + Environment.NewLine);
+            ShowArray(MyArray);
+
+            SortInsertion(MyArray);//<------
+
+            Console.Write(Environment.NewLine +
+                          Environment.NewLine +
+                          "Отсортированный массив" +
+                          Environment.NewLine);
+            ShowArray(MyArray);
+        }
+        else
+        {
+            if (MyArray.Length == 1)
+            {
+                Console.Write("Массив содержит 1 элемент" +
+                               Environment.NewLine +
+                               "Array" +
+                               Environment.NewLine);
+                ShowArray(MyArray);
+            }
+            else
+            {
+                Console.Write("Массив пуст");
+            }
+        }
+    }
+}
---
+++ Epam/Epam/Task3.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static public void MySplit(string str, List<string> MyArray)
+    {
+        string Word;
+        int IndexInarray = 0;
+        int Count = 0;
+        int Current;
+        if (str != "")
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != ' ')
+                {
+                    Current = i;
+                    //Выделяем слова и складываем в список
+                    while (i != str.Length && str[i] != ' ')
+                    {
+                        i++;
+                        Count++;
+                    }
+                    Word = str.Substring(Current, Count);
+                    Count = 0;
+
+                    MyArray.Add(Word);
+                    IndexInarray++;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Строка пустая");
+        }
+    }
+    static public void FindAllSingle(List<string>MyArray)
+    {
+        int CountSingleWords = 0;
+        int count;
+        if (MyArray.Count > 1)
+        {
+            foreach (string First in MyArray)
+            {
+                count = 0;
+                foreach (string Second in MyArray)
+                {
+                    if (count > 1)
+                    {
+                        break;
+                    }
+                    if (First == Second)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 1)
+                {
+                    CountSingleWords++;
+                    Console.Write($"{First} ");
+                }
+            }
+            if (CountSingleWords==0)
+            {
+                Console.WriteLine("Одиночный слов нет");
+            }
+        }
+        else
+        {
+            if (MyArray.Count == 1)
+            {
+                Console.Write($"{MyArray[0]} ");
+            }
+            else
+            {
+                Console.Write($"В этом списке слов нет!");
+            }
+
+        }
+        
+    }
+    //То,что это предложение и в нем содержаться зпятые, и прочие знаки припинания - не сказано
+    //По этому в качестве разделителя только пробел.
+
+    //А вообще если без этих велосипедов, то .Split потом .Join потом через Linq отсеиваем, Ps/можно еще Regex в качестве отсеивания прилепить.
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+
+        List<string> ArrayWords = new List<string>();
+        MySplit(str,ArrayWords);//Самописный сплит
+
+        FindAllSingle(ArrayWords);//Ищем все "одинокие" слова 
+    }
+}
