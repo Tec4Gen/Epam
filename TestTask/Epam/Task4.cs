7c46b14b37e064ce5641ece61291138e377c3616
Tec4Gen <steam.com77@yandex.ru>
2020-02-10 22:07:13 +0400

    Test Task upd

---
+++ TestTask/Epam/Task4.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static public ulong Fact(int n)
+    {
+        if (n < 0)
+        {
+            Console.WriteLine("n < 0 :*(");
+        }
+        if(n==0 || n==1)
+        {
+            return 1;
+        }
+
+        ulong Factorial = 1;
+        //чекаем на переполнение
+        checked
+        {
+            try
+            {
+                for (int i = 2; i <= n; i++)
+                {
+                    Factorial *= (ulong)i;
+                }
+                return Factorial;
+            }
+            catch
+            {
+                Console.WriteLine("n >= 21 :*(");
+                return 0;
+            }
+        }
+    } 
+    static void Main(string[] args)
+    {
+        int.TryParse(Console.ReadLine(), out int Number);
+
+        Console.Write($"Факториал {Number} = {Fact(Number)}");
+    }
+}
+
---
+++ TestTask/Epam/Task5.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Epam
+{
+    public class Task5
+    {
+        public static bool CheсkString(string str)
+        {
+            if (str.Length % 2 == 0 && str != "")
+            {
+                Stack<char> Stack_ = new Stack<char>();
+
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (Stack_.Count == 0)
+                    {
+                        if (str[i] != ')' && str[i] != ']' && str[i] != '}')
+                        {
+                            Stack_.Push(str[i]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Не правильная последовательность");
+                            return false;
+                        }
+
+                    }
+                    else
+                    {
+                        //Собираем все открывающие
+                        if (str[i] == '(')
+                        {
+                            Stack_.Push(str[i]);
+                        }
+                        else if (str[i] == '[')
+                        {
+                            Stack_.Push(str[i]);
+                        }
+                        else if (str[i] == '{')
+                        {
+                            Stack_.Push(str[i]);
+                        }
+                        else
+                        {
+                            //если некторой открывающей соответствует закрывающая вытаскиваем из стека
+                            if (str[i] != ')' || str[i] != ']' || str[i] != '}')
+                            {
+                                if (str[i] == ')' && Stack_.Peek()== '(')
+                                {
+                                    Stack_.Pop();
+                                }
+                                else if (str[i] == '}' && Stack_.Peek() == '{')
+                                {
+                                    Stack_.Pop();
+                                }
+                                else if (str[i] == ']' && Stack_.Peek() == '[')
+                                {
+                                    Stack_.Pop();
+                                }
+                            }   
+                        }
+                    } 
+                }
+                //Если стек пуст, значит ПСП 
+                if (Stack_.Count == 0)
+                {
+                    Console.WriteLine("Правильная последовательность");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Не правильная последовательность");
+                    return false;
+                }
+            }
+            else
+            {
+                if (str == "")
+                {
+                    Console.WriteLine("Строка пустая");
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Нечетное кол-во символов в строке не может быть ПСП");
+                    return false;
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            CheсkString(str);
+            //Если говорить о цункциях, то какое нибудь сложное регулярное выражение, легко бы заменило весь этот велосипед.
+        }
+    }
+}
