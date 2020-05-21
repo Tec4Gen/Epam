8bd56fc4430a71fef1b599d5947a74f02e3af4da
Tec4Gen <steam.com77@yandex.ru>
2020-05-21 21:38:19 +0400

    Task_1.1.3

---
+++ Task_1.1.3/StarPoint.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1._1._3
+{
+    class StarPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+
+
+            if (ulong.TryParse(Console.ReadLine(), out var Number))
+            {
+                Console.WriteLine("");
+                ShowTriangle(Number);
+            }
+            else
+            {
+                Console.WriteLine("Enter a positive number");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void ShowTriangle(ulong number)
+        {
+            if (number != 0)
+            {
+                for (ulong j = 1; number > 0; j+=2, number--)
+                {
+                    Console.WriteLine(new String(' ',(int)number) + new String('*', (int)j));
+                }
+            }
+            else
+            {
+                Console.WriteLine("is nothing to draw");
+            }
+        }
+    }
+}
