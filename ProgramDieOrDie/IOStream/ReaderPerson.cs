3d7fe8863fe29ee7e9ad9096aeef65916988bcff
tec4gen <steam.com77@yandex.ru>
2020-07-07 18:43:12 +0400

    Complete 3 1 1

---
+++ ProgramDieOrDie/IOStream/ReaderPerson.cs
@@ -0,0 +1,40 @@
+﻿using ProgramDieOrDie.Entityes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+namespace ProgramDieOrDie.IOStream
+{
+    public static class ReaderPerson
+    {
+        public static List<Person> Read(string path) 
+        {
+
+                using (StreamReader file = new StreamReader(path))
+                {
+                    List<Person> listPerson = new List<Person>();
+                    string line;
+
+                    string[] data;
+
+                    while (!file.EndOfStream)
+                    {
+                        line = file.ReadLine();
+                        data = line.Split(' ');
+
+                        listPerson.Add(new Person
+                        {
+                            FirstName = data[0],
+                            LastName = data[1],
+                        });
+                    }
+
+                    return listPerson;
+                }
+            
+ 
+                Console.WriteLine("File not found or path incorrect format");
+                return null;
+  
+        }
+    }
+}
---
+++ TestMyCouting/Run.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestMyCouting
+{
+    class Run
+    {
+        static void Main(string[] args)
+        {
+            LogicUI.Go(45);
+
+            Console.ReadLine();
+        }
+    }
+}
