23ffd6c3d5bcc4bba4d6b2bdde7df49e4a067036
tec4gen <steam.com77@yandex.ru>
2020-08-30 16:10:08 +0400

    8.5, landing, modal create,add notes

---
+++ WEB/Task2/Task_8.5/js/CreateNote.js
@@ -0,0 +1,60 @@
+import {Service} from "./MyArray.js";
+let ArrayNote = new Service();
+
+
+let modal = document.getElementById("ModalCreate");
+let mainBtn = document.getElementById("b__main__create");
+let mainBtn_create = document.getElementById("b__create");
+let mainBtn_close = document.getElementById("b__close");
+
+let bodyNode = document;
+
+mainBtn.onclick = function() {
+  modal.classList.toggle("btn__show");
+}
+
+mainBtn_create.onclick = function() {
+  let node = document.getElementById("base");
+  let temp = node.cloneNode(true);
+
+  let arrayHeadDivModal = bodyNode.getElementById("modal-content-head").childNodes;
+  let arrayBodyDivModal = bodyNode.getElementById("modal-content-body").childNodes;
+
+  let stringHeadText = "";
+  let stringBodyText = "";
+
+  let noteList = document.getElementById("notes__list");
+
+  noteList.append(temp);
+  temp.id = `note__item-${ArrayNote.id()}`
+
+  for (let index = 0; index < arrayHeadDivModal.length; index++) {
+
+    if (arrayHeadDivModal.length > 0) {
+      stringHeadText += arrayHeadDivModal[index].textContent + '<br>';
+    }
+  }
+  temp.querySelectorAll("span")[0].innerHTML = stringHeadText;
+
+  for (let index = 0; index < arrayBodyDivModal.length; index++) {
+
+    if (arrayBodyDivModal.length > 0) {
+      stringBodyText += arrayBodyDivModal[index].textContent + '<br>';
+    }
+  }
+  temp.querySelectorAll("span")[1].innerHTML = stringBodyText;
+
+  temp.querySelectorAll("span")[0].id = `content__head-text-${ArrayNote.id()}`;
+  temp.querySelectorAll("span")[1].id = `content__body-text-${ArrayNote.id()}`;
+  temp.querySelectorAll("button")[0].id = `note-deleted-buttom-${ArrayNote.id()}`;
+
+  ArrayNote.add(temp);
+
+  bodyNode.getElementById("modal-content-head").innerHTML = '';
+  bodyNode.getElementById("modal-content-body").innerHTML = '';
+
+}
+
+mainBtn_close.onclick = function() {
+  modal.classList.toggle("btn__show");
+}
---
+++ WEB/Task2/Task_8.5/js/MyArray.js
@@ -0,0 +1,88 @@
+export class Service {
+    #mapList = new Map();;
+    #id = 0;
+
+    add(value) {
+        if (value === undefined)
+            throw new SyntaxError(`value =>| invalid argument value`);
+
+        if (typeof (value) !== 'object')
+            throw new SyntaxError(`value =>| not object`);
+
+        this.#mapList.set(this.#id.toString(), value);
+        this.#id++;
+    }
+
+    id() {
+        return this.#id;
+    }
+    
+    show() {
+        this.#mapList.forEach((value, key) => {
+            console.log(`${key}: ${value}`);
+        });;
+    }
+
+    getById(keyString) {
+        if (keyString === undefined || keyString === null)
+            throw new SyntaxError(`value =>| invalid argument value`);
+
+        if (this.#mapList.has(keyString.toString())) {
+            return this.#mapList.get(keyString.toString());
+        }
+
+        return null;
+    }
+    deleteById(keyString) {
+        if (keyString === undefined || keyString === null)
+            throw new SyntaxError(`value =>| invalid argument value`);
+
+        if (this.#mapList.has(keyString.toString())) {
+            let result = this.#mapList.getById(keyString);
+            this.#mapList.delete(keyString.toString());
+
+            return result;
+        }
+
+        return null;
+    }
+
+    updateById(keyString, value) {
+        if ((keyString === undefined || keyString === null) ||
+            (value === undefined))
+            throw new SyntaxError(`value =>| invalid argument value`);
+
+        if (typeof (value) !== 'object')
+            throw new SyntaxError(`value =>| not object`);
+
+        if (this.#mapList.has(keyString.toString())) {
+            let obj = this.getById(keyString);
+            obj = Object.assign(obj,value);
+
+            this.replaceById(keyString, obj);
+            return true;
+        }
+        return false;
+    }
+
+    replaceById(keyString, value) {
+        if ((keyString === undefined || keyString === null) ||
+            (value === undefined))
+            throw new SyntaxError(`value =>| invalid argument value`);
+
+        if (typeof (value) !== 'object')
+            throw new SyntaxError(`value =>| not object`);
+
+        if (this.#mapList.has(keyString.toString())) {
+            this.#mapList.set(keyString.toString(), value);
+            return true;
+        }
+        return false;
+    }
+
+    getAll() {
+        return Array.from(this.#mapList.values());
+    }
+}
+
+
